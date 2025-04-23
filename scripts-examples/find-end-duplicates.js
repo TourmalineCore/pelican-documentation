9597a11d044c0477a4d7f2c0899d324b0a7e88b9
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-04-23 15:15:55 +0500

    feat: add find end duplicates in urls example

---
+++ scripts-examples/find-end-duplicates.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-await-in-loop */
+
+// in this repo you can't run it, this is just an example for the future
+
+// This is one-time script for find end duplicates in news archive, without refactoring
+
+// to run enter command in terminal:
+// node find-end-duplicates.js
+// dont forget to add file to tsconfig.json:
+// "include": ["find-end-duplicates.js"]
+
+const fs = require(`fs`);
+
+fs.readFile(`zoo_news_archive.json`, `utf8`, (err, data) => {
+  const jsonData = JSON.parse(data);
+  const urlCounts = {};
+
+  jsonData.forEach((item) => {
+    const {
+      url,
+    } = item;
+
+    const lastSlashIndex = url.lastIndexOf(`/`);
+    const secondLastSlashIndex = url.lastIndexOf(`/`, lastSlashIndex - 1);
+
+    const urlEnd = url.substring(secondLastSlashIndex);
+
+    if (urlCounts[urlEnd]) {
+      urlCounts[urlEnd]++;
+    } else {
+      urlCounts[urlEnd] = 1;
+    }
+  });
+
+  const resultArray = Object.entries(urlCounts)
+    .filter(([_, count]) => count > 1)
+    .map(([urlEnd, count]) => ({
+      urlEnd,
+      count,
+    }));
+
+  fs.writeFileSync(`url_counts.json`, JSON.stringify(resultArray, null, 2));
+});
